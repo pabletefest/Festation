12f95a3c0645a26f39e1ff5848593ae72521ba42
PabloSF <62628314+pabletefest@users.noreply.github.com>
2024-08-07 07:31:15 +0200

    WIP mips decoding phase

--- src/cpu/psx_cpu.cpp
+++ src/cpu/psx_cpu.cpp
@@ -1,1 +1,140 @@
 #include "psx_cpu.h"
+#include "mips_r3000a_opcodes.h"
+
+#include <cstring>
+#include <stdio.h>
+
+namespace festation
+{
+    #define INSTRUCTION_SIZE 4
+
+    PSXRegs r3000a_regs;
+};
+
+
+festation::MIPS_R3000A::MIPS_R3000A()
+    : tempRAM(2 * 1024 * 1024)
+{
+    std::memset((void*) &r3000a_regs, 0, sizeof(PSXRegs));
+}
+
+uint8_t festation::MIPS_R3000A::read8(uint32_t address)
+{
+    return tempRAM[address];
+}
+
+uint16_t festation::MIPS_R3000A::read16(uint32_t address)
+{
+    return *(uint16_t*)&tempRAM.data()[address];
+}
+
+uint32_t festation::MIPS_R3000A::read32(uint32_t address)
+{
+    return *(uint32_t*)&tempRAM.data()[address];
+}
+
+void festation::MIPS_R3000A::write8(uint32_t address, uint8_t value)
+{
+    tempRAM[address] = value;
+}
+
+void festation::MIPS_R3000A::write16(uint32_t address, uint16_t value)
+{
+    *(uint16_t*)&tempRAM.data()[address] = value;
+}
+
+void festation::MIPS_R3000A::write32(uint32_t address, uint32_t value)
+{
+    *(uint32_t*)&tempRAM.data()[address] = value;
+}
+
+void festation::MIPS_R3000A::executeInstruction()
+{
+    uint8_t instruction = fetchInstruction();
+
+    InstructionType instructionType = decodeInstruction(instruction);
+
+    EncodingType type = std::get<0>(instructionType);
+
+    InstructionTypePtr instructionTypePtr = std::get<1>(instructionType);
+
+    switch (type)
+    {
+    case EncodingType::REGISTER:
+        {
+            RTypeInstructionEncoding instructionEncoding = std::get<EncodingType::REGISTER>(instructionTypePtr);
+            auto instructionCodePtr = std::get<0>(instructionEncoding);
+            uint8_t rd = std::get<1>(instructionEncoding);
+            uint8_t rt = std::get<2>(instructionEncoding);
+            uint8_t rs = std::get<3>(instructionEncoding);
+            uint8_t shiftAmount = std::get<4>(instructionEncoding);
+
+            instructionCodePtr(rd, rt, rs, shiftAmount);
+        }
+        break;
+    case EncodingType::IMMEDIATE:
+        {
+            ITypeInstructionEncoding instructionEncoding = std::get<EncodingType::IMMEDIATE>(instructionTypePtr);
+            auto instructionCodePtr = std::get<0>(instructionEncoding);
+            uint8_t rt = std::get<1>(instructionEncoding);
+            uint8_t rs = std::get<2>(instructionEncoding);
+            uint16_t immed16 = std::get<3>(instructionEncoding);
+
+            instructionCodePtr(rt, rs, immed16);
+        }
+        break;
+    case EncodingType::JUMP:
+        {
+            JTypeInstructionEncoding instructionEncoding = std::get<EncodingType::JUMP>(instructionTypePtr);
+            auto instructionCodePtr = std::get<0>(instructionEncoding);
+            uint32_t immed32 = std::get<1>(instructionEncoding);
+
+            instructionCodePtr(immed32);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+uint32_t festation::MIPS_R3000A::fetchInstruction()
+{
+    uint32_t instruction = read32(r3000a_regs.pc);
+
+    r3000a_regs.pc += INSTRUCTION_SIZE;
+
+    return instruction;
+}
+
+festation::InstructionType festation::MIPS_R3000A::decodeInstruction(uint32_t instruction)
+{
+    uint8_t opcode = getInstOpcode(instruction);
+
+    if (opcode == R_TYPE_OPCODE_PATTERN) // R-FORMAT INSTRUCTION
+    {
+        return std::make_pair<EncodingType, InstructionTypePtr>(EncodingType::REGISTER, decodeRFormat(instruction));
+    }
+    else if ((opcode & J_TYPE_OPCODE_MASK) >= J_TYPE_OPCODE_PATTERN) // J-FORMAT INSTRUCTION
+    {
+        return std::make_pair<EncodingType, InstructionTypePtr>(EncodingType::IMMEDIATE, decodeIFormat(instruction));
+    }
+    else // I-FORMAT INSTRUCTION
+    {
+        return std::make_pair<EncodingType, InstructionTypePtr>(EncodingType::JUMP, decodeJFormat(instruction));
+    }
+}
+
+festation::InstructionTypePtr festation::MIPS_R3000A::decodeRFormat(uint32_t instruction)
+{
+    return InstructionTypePtr();
+}
+
+festation::InstructionTypePtr festation::MIPS_R3000A::decodeJFormat(uint32_t instruction)
+{
+    return InstructionTypePtr();
+}
+
+festation::InstructionTypePtr festation::MIPS_R3000A::decodeIFormat(uint32_t instruction)
+{
+    return InstructionTypePtr();
+}
--- src/cpu/psx_cpu.h
+++ src/cpu/psx_cpu.h
@@ -1,7 +1,11 @@
 #pragma once
-#include "mips_r300a_state.h"
+#include "mips_r3000a_state.h"
+#include "cpu_masks_types_utils.h"
 
 #include <cstdint>
+
+// TEMP
+#include <vector>
 
 namespace festation
 {
@@ -11,14 +15,31 @@ namespace festation
     class MIPS_R3000A
     {
     public:
-        MIPS_R3000A() = default;
+        MIPS_R3000A();
         ~MIPS_R3000A() = default;
 
+        uint8_t read8(uint32_t address);
+        uint16_t read16(uint32_t address);
+        uint32_t read32(uint32_t address);
+        
+        void write8(uint32_t address, uint8_t value);
+        void write16(uint32_t address, uint16_t value);
+        void write32(uint32_t address, uint32_t value);
+
+        void executeInstruction();
         void clockCycles(uint32_t cycles);
+
+    private:        
+        uint32_t fetchInstruction();
+        InstructionType decodeInstruction(uint32_t instruction);
+        InstructionTypePtr decodeRFormat(uint32_t instruction);
+        InstructionTypePtr decodeJFormat(uint32_t instruction);
+        InstructionTypePtr decodeIFormat(uint32_t instruction);
 
     private:
         uint64_t totalCyclesElapsed;
 
-        PSXRegs r3000a_regs;
+        // TEMP
+        std::vector<uint8_t> tempRAM;
     };
 };
