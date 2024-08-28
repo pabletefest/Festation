f3600263c7db655d1440a67544312d730c9d0fe0
PabloSF <62628314+pabletefest@users.noreply.github.com>
2024-08-28 20:11:27 +0200

    BIOS read/write functions implemented

--- src/kernel_bios/bios.cpp
+++ src/kernel_bios/bios.cpp
@@ -10,32 +10,32 @@ namespace festation
 
 uint8_t festation::KernelBIOS::read8(uint32_t address)
 {
-    return 0;
+    return biosROM[address];
 }
 
 uint16_t festation::KernelBIOS::read16(uint32_t address)
 {
-    return 0;
+    return *(uint16_t*)&biosROM[address];
 }
 
 uint32_t festation::KernelBIOS::read32(uint32_t address)
 {
-    return 0;
+    return *(uint32_t*)&biosROM[address];
 }
 
 void festation::KernelBIOS::write8(uint32_t address, uint8_t value)
 {
-    
+    biosROM[address] = value;
 }
 
 void festation::KernelBIOS::write16(uint32_t address, uint16_t value)
 {
-
+    *(uint16_t*)&biosROM[address] = value;
 }
 
 void festation::KernelBIOS::write32(uint32_t address, uint32_t value)
 {
-
+    *(uint32_t*)&biosROM[address] = value;
 }
 
 bool festation::KernelBIOS::loadBIOSROMFile(const std::string &filename)
