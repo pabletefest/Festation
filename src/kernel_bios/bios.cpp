61624bf57c5c6f9d57bbf9d9ac8a743a4bc71c7c
PabloSF <62628314+pabletefest@users.noreply.github.com>
2024-08-28 18:29:05 +0200

    KernelBIOS object created and BIOS ROM loading function added

--- src/kernel_bios/bios.cpp
+++ src/kernel_bios/bios.cpp
@@ -1,1 +1,61 @@
 #include "bios.hpp"
+
+#include <fstream>
+#include <cassert>
+
+namespace festation
+{
+    static constexpr const uint32_t BIOS_SIZE = 512 * 1024;
+};
+
+uint8_t festation::KernelBIOS::read8(uint32_t address)
+{
+    return 0;
+}
+
+uint16_t festation::KernelBIOS::read16(uint32_t address)
+{
+    return 0;
+}
+
+uint32_t festation::KernelBIOS::read32(uint32_t address)
+{
+    return 0;
+}
+
+void festation::KernelBIOS::write8(uint32_t address, uint8_t value)
+{
+    
+}
+
+void festation::KernelBIOS::write16(uint32_t address, uint16_t value)
+{
+
+}
+
+void festation::KernelBIOS::write32(uint32_t address, uint32_t value)
+{
+
+}
+
+bool festation::KernelBIOS::loadBIOSROMFile(const std::string &filename)
+{
+    std::ifstream file;
+
+    file.open(filename, std::ios::in | std::ios::binary);
+
+    if (file.is_open())
+    {
+        file.seekg(std::ios::end);
+        std::size_t fileSize = file.tellg();
+        file.seekg(std::ios::beg);
+
+        assert((fileSize != BIOS_SIZE) && "Provided BIOS ROM file doesn't match proper PSX BIOS file size (512KB)!");
+
+        file.read((char*)biosROM.data(), fileSize);
+
+        return true;
+    } 
+
+    return false;
+}
--- src/kernel_bios/bios.hpp
+++ src/kernel_bios/bios.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
 namespace festation
 {
+    class KernelBIOS
+    {
+    public:
+        KernelBIOS() = default;
+        ~KernelBIOS() = default;
 
+        uint8_t read8(uint32_t address);
+        uint16_t read16(uint32_t address);
+        uint32_t read32(uint32_t address);
+        
+        void write8(uint32_t address, uint8_t value);
+        void write16(uint32_t address, uint16_t value);
+        void write32(uint32_t address, uint32_t value);
+
+        bool loadBIOSROMFile(const std::string& filename);
+
+        inline std::vector<uint8_t>& getBIOSData() { return biosROM; }
+    
+    private:
+        std::vector<uint8_t> biosROM;
+    };
 };
