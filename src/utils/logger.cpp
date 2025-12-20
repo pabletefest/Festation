9b55bb29e0b3ab7411289175f82386fbc2cc30b7
pabletefest <stemmerpablo@gmail.com>
2025-12-20 13:04:30 +0100

    Logger improved with colors

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -5,6 +5,29 @@
 
 static std::mutex s_loggerMutex;
 
+enum class OutputColor {
+	DEFAULT,
+	YELLOW,
+	RED
+};
+
+static void printColoredLine(OutputColor color, std::string& message) {
+	switch (color) {
+	case OutputColor::DEFAULT:
+		std::println("{}", message);
+		break;
+	case OutputColor::YELLOW:
+		std::println("\033[33m{}\033[0m", message);
+		break;
+	case OutputColor::RED:
+		std::println("\033[31m{}\033[0m", message);
+		break;
+	default:
+		std::println("{}", message);
+		break;
+	}
+}
+
 //static Logger::LogLevel s_logLevel = Logger::LogLevel::DEBUG;
 
 void Logger::log(Logger::LogLevel level, const std::string& message)
@@ -16,11 +39,25 @@ void Logger::log(Logger::LogLevel level, const std::string& message)
 
 	switch (level)
 	{
+	case LogLevel::DEBUG:
+		printColoredLine(OutputColor::DEFAULT, std::string("[DEBUG] - ").append(message));
+		break;
+	case LogLevel::INFO:
+		printColoredLine(OutputColor::DEFAULT, std::string("[INFO] - ").append(message));
+		break;
+	case LogLevel::WARN:
+		printColoredLine(OutputColor::YELLOW, std::string("[WARN] - ").append(message));
+		break;
+	case LogLevel::ERROR:
+		printColoredLine(OutputColor::RED, std::string("[ERROR] - ").append(message));
+		break;
+	case LogLevel::CRITICAL:
+		printColoredLine(OutputColor::RED, std::string("[CRITICAL] - ").append(message));
+		break;
 	default:
+		std::println("UNSUPPORTED LOG LEVEL!");
 		break;
 	}
-
-	std::println("{}", message);
 }
 
 //Logger& Logger::getLogger()
