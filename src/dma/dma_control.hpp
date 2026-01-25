adae727f8fa155d6d64837d2a34f3c1631f36315
pabletefest <stemmerpablo@gmail.com>
2026-01-25 16:53:26 +0100

    DMA control registers details implemented

--- src/dma/dma_control.hpp
+++ src/dma/dma_control.hpp
@@ -6,7 +6,7 @@
 #include <array>
 
 namespace festation {
-    inline constexpr uint8_t DMA_CHANNELS = 6u;
+    inline constexpr uint8_t DMA_CHANNELS = 7u;
 
     class DmaControl {
     public:
@@ -19,7 +19,22 @@ namespace festation {
     private:
         union DmaControlRegister {
             struct {
-
+                uint32_t mdecInPriority : 3;
+                uint32_t mdecInMasterEnable : 1;
+                uint32_t mdecOutPriority : 3;
+                uint32_t mdecOutMasterEnable : 1;
+                uint32_t gpuPriority : 3;
+                uint32_t gpuMasterEnable : 1;
+                uint32_t cdromPriority : 3;
+                uint32_t cdromMasterEnable : 1;
+                uint32_t spuPriority : 3;
+                uint32_t spuMasterEnable : 1;
+                uint32_t pioPriority : 3;
+                uint32_t pioMasterEnable : 1;
+                uint32_t otcPriority : 3;
+                uint32_t otcMasterEnable : 1;
+                uint32_t cpuMemoryAccessPriority : 3;
+                uint32_t cpuMemoryAccessEnable : 1; // Docs say: No effect, should be CPU memory access enable (R/W)
             };
 
             uint32_t raw;
@@ -27,7 +42,13 @@ namespace festation {
 
         union DmaInterruptRegister {
             struct {
-
+                uint32_t channelsCompletionInterruptsControl : 7;
+                uint32_t unused : 8;
+                uint32_t busErrorFlag : 1;
+                uint32_t channelsInterruptMask : 7;
+                uint32_t masterChannelInterruptEnable : 1;
+                uint32_t channelsInterruptFlags : 7;
+                uint32_t masterInterruptFlag : 1;
             };
             
             uint32_t raw;
