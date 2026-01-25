a3869186427e18a04eab46942e11d06e170bd83f
pabletefest <stemmerpablo@gmail.com>
2026-01-25 12:21:26 +0100

    DmaChannels registers bitfields implemented

--- src/dma/dma_channel.hpp
+++ src/dma/dma_channel.hpp
@@ -14,15 +14,26 @@ namespace festation {
     private:
         union DmaBaseAddress {
             struct {
-
+                uint32_t startMemAddr : 24;
+                uint32_t unused : 8;
             };
 
             uint32_t raw;
         } Dx_MADR;
 
         union DmaBlockControl {
-            struct {
+            struct BcrSyncMode0 {
+                uint32_t wordsNumber : 16;
+                uint32_t unused : 16;
+            };
 
+            struct BcrSyncMode1 {
+                uint32_t blockSize : 16;
+                uint32_t blocksAmount : 16;
+            };
+
+            struct BcrSyncMode2 {
+                uint32_t unused;
             };
             
             uint32_t raw;
@@ -30,7 +41,22 @@ namespace festation {
 
         union DmaChannelControl {
             struct {
-
+                uint32_t transferDirection : 1;
+                uint32_t madrIncrementPerStep : 1;
+                uint32_t unused : 6;
+                uint32_t modeEffect : 1;
+                uint32_t transferSyncMode : 2;
+                uint32_t unused2 : 5;
+                uint32_t choppingDmaWindowSize : 3;
+                uint32_t unused3 : 1;
+                uint32_t choppingCpuWindowSize : 3;
+                uint32_t unused4 : 1;
+                uint32_t startTransfer : 1;
+                uint32_t unused5 : 3;
+                uint32_t forceTransferStartWithoutDREQWaiting : 1;
+                uint32_t modeEffect2 : 1;
+                uint32_t performBusSnooping : 1;
+                uint32_t unused6 : 1;
             };
             
             uint32_t raw;
