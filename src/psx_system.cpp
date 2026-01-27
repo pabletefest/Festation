982aedd7321b4806422abaea0285092acde3882c
pabletefest <stemmerpablo@gmail.com>
2026-01-27 22:53:54 +0100

    DMA reset added

--- src/psx_system.cpp
+++ src/psx_system.cpp
@@ -21,6 +21,7 @@ festation::PSXSystem::~PSXSystem()
 void festation::PSXSystem::reset()
 {
     cpu.reset();
+    dma.reset();
 }
 
 // IMPLEMENT READ16 AND READ32 AS MULTIPLE READ8 SIMPLIFIES IMPLEMENTATION
@@ -36,23 +37,22 @@ uint8_t festation::PSXSystem::read8(uint32_t address)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 1!", masked_address);
         return 0xFF; // Stub
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        LOG_DEBUG("Read8 from I/O port address 0x{:08X}", address);
-        //assert(false && "Not implemented read on I/O Ports!");
+        LOG_DEBUG("Read8 from I/O port address 0x{:08X}", masked_address);
         return 0xFF;
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 2!", masked_address);
         return 0xFF;
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented read8 at 0x{:08X} on expansion region 3!", masked_address);
         return 0xFF;
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
@@ -77,23 +77,22 @@ uint16_t festation::PSXSystem::read16(uint32_t address)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 1!", masked_address);
         return 0xFFFF; // Stub
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        LOG_DEBUG("Read16 from I/O port address 0x{:08X}", address);
-        //assert(false && "Not implemented read on I/O Ports!");
+        LOG_DEBUG("Read16 from I/O port address 0x{:08X}", masked_address);
         return 0xFFFF;
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 2!", masked_address);
         return 0xFFFF;
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented read16 at 0x{:08X} on expansion region 3!", masked_address);
         return 0xFFFF;
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
@@ -118,35 +117,46 @@ uint32_t festation::PSXSystem::read32(uint32_t address)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 1!", masked_address);
         return 0xFFFFFFFF; // Stub
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        switch(masked_address) {
+        uint32_t readValue = 0xFFFFFFFF;
+
+        switch(masked_address)
+        {
         case 0x1F801810:
         case 0x1F801814:
         {
-            uint32_t readValue = gpu.read32(masked_address);
-            LOG_DEBUG("Reading {:08X}h from GPU IO port 0x{:08X}", readValue, address);
-            return readValue;
+            readValue = gpu.read32(masked_address);
+            LOG_DEBUG("Reading {:08X}h from GPU IO port 0x{:08X}", readValue, masked_address);
+            break;
         }
         default:
-            LOG_DEBUG("Read32 from I/O port address 0x{:08X}", address);
+            if (masked_address >= 0x1F801080 && masked_address <= 0x1F8010FF)
+            {
+                readValue = dma.read32(masked_address);
+                LOG_DEBUG("Reading {:08X}h from DMA IO port 0x{:08X}", readValue, masked_address);
+            }
+            else
+            {
+                LOG_DEBUG("Read32 from I/O port address 0x{:08X}", masked_address);
+            }
+
             break;
         }
 
-        //assert(false && "Not implemented read on I/O Ports!");
-        return 0xFFFFFFFF;
+        return readValue;
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 2!", masked_address);
         return 0xFFFFFFFF;
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented read32 at 0x{:08X} on expansion region 3!", masked_address);
         return 0xFFFFFFFF;
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
@@ -174,20 +184,19 @@ void festation::PSXSystem::write8(uint32_t address, uint8_t value)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 1!", masked_address);
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        LOG_DEBUG("Write8 ({:02X}h) to I/O port address 0x{:08X}", value, address);
-        //assert(false && "Not implemented write on I/O Ports!");
+        LOG_DEBUG("Write8 ({:02X}h) to I/O port address 0x{:08X}", value, masked_address);
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 2!", masked_address);
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented write8 to 0x{:08X} on expansion region 3!", masked_address);
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
     {
@@ -212,20 +221,19 @@ void festation::PSXSystem::write16(uint32_t address, uint16_t value)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 1!", masked_address);
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        LOG_DEBUG("Write16 ({:04X}h) to I/O port address 0x{:08X}", value, address);
-        //assert(false && "Not implemented write on I/O Ports!");
+        LOG_DEBUG("Write16 ({:04X}h) to I/O port address 0x{:08X}", value, masked_address);
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 2!", masked_address);
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented write16 to 0x{:08X} on expansion region 3!", masked_address);
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
     {
@@ -250,30 +258,38 @@ void festation::PSXSystem::write32(uint32_t address, uint32_t value)
     }
     else if (masked_address >= EXPANSION_REGION1_START && masked_address <= EXPANSION_REGION1_END)
     {
-        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 1!", address);
+        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 1!", masked_address);
     }
     else if (masked_address >= IO_PORTS_START && masked_address <= IO_PORTS_END)
     {
-        switch(masked_address) {
+        switch(masked_address)
+        {
         case 0x1F801810:
         case 0x1F801814:
-            LOG_DEBUG("Writting {:08X}h to GPU IO port 0x{:08X}", value, address);
+            LOG_DEBUG("Writting {:08X}h to GPU IO port 0x{:08X}", value, masked_address);
             gpu.write32(masked_address, value);
             break;
         default:
-            LOG_DEBUG("Write32 ({:08X}h) to I/O port address 0x{:08X}", value, address);
+            if (masked_address >= 0x1F801080 && masked_address <= 0x1F8010FF)
+            {
+                LOG_DEBUG("Writting {:08X}h to DMA IO port 0x{:08X}", value, masked_address);
+                dma.write32(masked_address, value);
+            } 
+            else
+            {
+                LOG_DEBUG("Write32 ({:08X}h) to I/O port address 0x{:08X}", value, masked_address);
+            }
+
             break;
         }
-
-        //assert(false && "Not implemented write on I/O Ports!");
     }
     else if (masked_address >= EXPANSION_REGION2_START && masked_address <= EXPANSION_REGION2_END)
     {
-        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 2!", address);
+        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 2!", masked_address);
     }
     else if (masked_address >= EXPANSION_REGION3_START && masked_address <= EXPANSION_REGION3_END)
     {
-        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 3!", address);
+        LOG_WARN("Not implemented write32 to 0x{:08X} on expansion region 3!", masked_address);
     }
     else if (masked_address >= BIOS_ROM_START && masked_address <= BIOS_ROM_END)
     {
@@ -330,5 +346,5 @@ void festation::PSXSystem::sideloadExeFile(const std::filesystem::path& path)
     std::memcpy(mainRAM.data() + startExeRamAddress, 
         exe.data() + HEADER_SIZE, exeSize);
 
-    cpu.getCPURegs().pc = initialPC;
+    pcRef = initialPC;
 }
--- src/psx_system.hpp
+++ src/psx_system.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "psx_cw33300_cpu.hpp"
 #include "kernel_bios/bios.hpp"
+#include "dma/dma_control.hpp"
 #include "gpu/gpu.hpp"
 
 #include <vector>
@@ -32,6 +33,7 @@ namespace festation
         //uint8_t* mainRAM = nullptr;
         std::vector<uint8_t> mainRAM;
         KernelBIOS bios;
+        DmaControl dma;
         PsxGpu gpu;
         uint64_t totalElapsedCycles;
     };
